701805532f8f0e8b5c59407d78bbf5318d36e104
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2024-05-14 16:26:05 +0800

    wala lang

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -6,12 +6,12 @@ function Account() {
   return (
     <>
       <Heading as='h1'>Update your account</Heading>
-
+      
       <Row>
         <Heading as='h3'>Update user data</Heading>
         <p>Update user data form</p>
       </Row>
-
+    
       <Row>
         <Heading as='h3'>Update password</Heading>
         <p>Update user password form</p>
